8210d02362d58b1ef6a5ee44869aecebcbaed627
Navneet Prakash <navprak@gmail.com>
2016-02-19 23:18:27 +1300

    implemented start new round method and target label update

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //method to start the round and gen new random number
+
+        startNewRound()
+        updateLabels()
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction  func showAlert() {
+        
+        let message = "The value of slider is \(currentValue)" + "\n The target value is: \(targetValue)"
+        
+        //alert messasge window display
+        let alert = UIAlertController(title: "Check Result", message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        //present the alert
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        updateLabels()
+    
+    }
+    
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        
+        //store current value of slider to variable
+        currentValue = lroundf(slider.value)
+    
+    }
+    
+    func startNewRound() {
+        
+        //        targetValue -  the highest number you will get is 99 because arc4random_uniform() treats
+        //        the upper limit as exclusive. It only goes up-to 100, not up-to-and-including. To get
+        //        a number that is truly in the range 1 - 100, you need to add 1 to the result of
+        //        arc4random_uniform().
+        
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        
+        //Put the UISlider back to Float that is the currentValue
+        slider.value = Float(currentValue)
+        
+        updateLabels()
+    }
+    
+    func updateLabels(){
+            
+            targetLabel.text = String(targetValue)
     }
 
-
 }
-
